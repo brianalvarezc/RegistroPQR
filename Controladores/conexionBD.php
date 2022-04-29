77cafea2ea99313b33de3c7c1b506acc74517782
brianalvarezc <brian.alvarez.cuadros@gmail.com>
2022-04-29 16:18:34 -0500

    Correccion de error en código comentado

--- Controladores/conexionBD.php
+++ Controladores/conexionBD.php
@@ -1,16 +1,16 @@
 <?php
 
-	// // Se realiza la conexion con la Base de datos tomando mysqli como una clase
-	// $host = 'localhost';
+	// Se realiza la conexion con la Base de datos tomando mysqli como una clase
+	$host = 'localhost';
 
-	// // Local
-	// $user = 'root';
-	// $pass = '';
+	// Local
+	$user = 'root';
+	$pass = '';
 
-	// $basedatos = 'registro_pqr';
+	$basedatos = 'registro_pqr';
 
-	// $conexion = new mysqli($host, $user, $pass, $basedatos);
-	// if($conexion -> connect_errno){
-	// 	die("Fallo la conexion: (" . $conexion -> connect_errno . ")" . $conexion -> mysqli_connect_error);
-	// }
+	$conexion = new mysqli($host, $user, $pass, $basedatos);
+	if($conexion -> connect_errno){
+		die("Fallo la conexion: (" . $conexion -> connect_errno . ")" . $conexion -> mysqli_connect_error);
+	}
 ?>
